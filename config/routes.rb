a6ae6d2883da509dcb565765db186e7ffab755d5
emily doswell <emilydoswell@yahoo.co.uk>
2021-07-17 12:58:27 +0200

    added contacts page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/project',  to: "projects#project"
+  get '/new', to: "contacts#new"
 end
